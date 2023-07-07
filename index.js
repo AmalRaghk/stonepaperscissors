d51da8525a76691d0002d82c53892421652af096
Amal Ragh K <amalraghk123@gmail.com>
2023-07-07 11:32:17 +0530

    logic implemented

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ let computerChoices=['stone','paper','scissors'];
 let computerchooser=Math.random();
 let userChoice=[];
 let winner='player';
+const lifeNos=document.querySelector('.lifenos');
+const decide=(i)=>{
+  if (i<=3){
+    i=0;
+  }
+  else if(i<=6){
+    i=1;
+  }
+  else{
+    i=2;
+  }
+  return i
+  
+}
 
 const printLife=()=>{
-    const lifeNos=document.querySelector('.lifenos');
-    for (let i=0;i<totallife;i++)
-    {
-        const lifeImage=document.createElement('img');
-        lifeImage.setAttribute('class','lifeImage')
-        lifeImage.src='./images/heart.png';
-        lifeNos.appendChild(lifeImage);
-
-    }
+  lifeNos.innerHTML = '';
+  for (let i = 0; i < totallife; i++) {
+    const lifeImage = document.createElement('img');
+    lifeImage.setAttribute('class', 'lifeImage');
+    lifeImage.src = './images/heart.png';
+    lifeNos.appendChild(lifeImage);
+  }
 }
 
 const comments=()=>{
@@ -33,17 +45,14 @@ let choice='';
 function slot() {
   const slotElement = document.querySelector('.slot');
   let i=Math.random()*10;
-  if (i<=3){
-    i=0;
-  }
-  else if(i<=6){
-    i=1;
-  }
-  else{
-    i=2;
-  }
+  i=decide(i);
+  const img=document.createElement('img');
+  slotElement.appendChild(img);
   intervalId = setInterval(() => {
-    slotElement.innerHTML = `<img src="${computerChoiceImages[i]}">`;
+    const img=document.createElement('img');
+    img.src=computerChoiceImages[i];
+    img.setAttribute('id',i);
+    slotElement.replaceChild(img, slotElement.firstChild);
     i = (i + 1) % computerChoiceImages.length;
   }, 1000);
 }
@@ -54,15 +63,52 @@ function stop() {
 }
 
 //document.querySelector('.stop-button').addEventListener('click', stop);
-const winners=()=>{
+const winners = (id) => {
+  const slotElement = document.querySelector('.slot');
+  const computersmove = decide(Math.random())
+  const img=document.createElement('img');
+  img.src=computerChoiceImages[computersmove];
+  slotElement.replaceChild(img, slotElement.firstChild);
+  console.log(computersmove,id);
 
-  
+  if (id == computersmove) {
+    winner = 'Tie';
+  } else if (
+    (id == 0 && computersmove == 2) ||
+    (id == 1 && computersmove == 0) ||
+    (id == 2 && computersmove == 1)
+  ) {
+    winner = 'player';
+    totallife--;
+  } else {
+    winner = 'computer';
+    totallife--;
+  }
+  comments();
+  printLife();
+};
+
+const usersChoice=()=>{
+  const options=document.querySelector('.options');
+  for(let i=0;i<3;i++){
+    const img=document.createElement('img');
+    img.setAttribute('id',i);
+    const choice=document.createElement('div');
+    choice.setAttribute('class','choice'+i);
+    img.src=computerChoiceImages[i];
+    choice.appendChild(img)
+    options.appendChild(choice)
+    img.onclick=()=>{
+      stop();
+      winners(img.id)
+    }
+  }
 
 }
 printLife();
-totallife=2;
 slot();
 comments();
+usersChoice();
 
 
 
