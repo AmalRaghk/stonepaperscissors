f98e94af585ae0ab92018d32d9600b923cd88d58
Amal Ragh K <amalraghk123@gmail.com>
2023-07-10 13:32:48 +0530

    added overlay

--- index.js
+++ index.js
@@ -57,7 +57,18 @@ function winnerImageShower(roundWinner) {
   }
 }
 function championEffect(){
-  document.body.innerHTML = '';
+  const overlay = document.createElement('div');
+  overlay.setAttribute('class','disChamp')
+  overlay.style.position = 'fixed';
+  overlay.style.top = 0;
+  overlay.style.left = 0;
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  overlay.innerHTML=champion;
+  document.body.appendChild(overlay);
+  
+
 }
 const lifeimagefunction = () => {
   let lifeimages = ``;
